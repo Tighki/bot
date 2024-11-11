8f23d8dc51183eb08f096714c3757cf1fd66cfc5
Tighki <115498774+Tighki@users.noreply.github.com>
2024-11-11 15:20:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import rclpy
 from drone_controller.controller import DroneController
+from drone_controller.land import LandManager
+from rclpy.node import Node
 
 def main(args=None):
+    # Инициализация rclpy
     rclpy.init(args=args)
+
+    # Создание экземпляра DroneController
     drone_controller = DroneController()
+
+    # Создание экземпляра Node для LandManager
+    land_manager_node = Node("land_manager_node")
+    land_manager = LandManager(land_manager_node)
     rclpy.spin(drone_controller)
+    # Уничтожение узлов при выходе
     drone_controller.destroy_node()
-    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
